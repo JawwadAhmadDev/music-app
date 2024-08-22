e3fd6f5432ef4067d4d4a8e1c8a20946d94d4fa5
Jawwad Ahmad <109460336+JawwadAhmadDev@users.noreply.github.com>
2024-08-22 20:06:31 +0500

    featured courses section completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
+      <FeaturedCourses />
     </div>
   );
 }
